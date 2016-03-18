493e9936401b7413720f05c52f6fcc0830b411a2
SarahTierney <tierney.s@live.com>
2016-03-18 10:20:19 -0400

    finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var $input = $('#todo-input');
-var $list = $(".list");
+var $list = $('.list');
 
-$('#todo-list').on('submit', function (e) {
+$('form').on('submit', function (e) {
   var $li = $('<li>');
+
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+  $input.val('');
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('todo-input');
 });
